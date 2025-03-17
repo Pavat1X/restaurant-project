21fa84c2ed689e92c2287411ac611c8a6935b2b6
pavat1x <pavatpichetsin@outlook.com>
2025-03-17 12:56:17 +0000

    refactored to use less code

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,23 +5,15 @@
 const pageHero = document.createElement('div');
 pageHero.setAttribute('id', 'page-hero');
 
-const heroText = document.createElement('div');
-const heroTextMain = document.createElement('p');
-const heroTextSub = document.createElement('p');
-
-heroText.setAttribute('id', 'hero-text');
-heroTextMain.setAttribute('id', 'hero-text-main');
-heroTextSub.setAttribute('id', 'hero-text-sub');
-
-heroTextMain.innerHTML = 'A <span>Premier</span> Cantonese Restaurant';
-heroTextSub.innerHTML = 'serving authentic Hong Kong style cuisine';
-
-heroText.appendChild(heroTextMain, heroTextSub);
-pageHero.appendChild(heroText);
+pageHero.innerHTML = '<div id="hero-text"><p id="hero-text-main">A <span>Premier</span> Cantonese Restaurant</p><p id="hero-text-sub">serving authentic Hong Kong style cuisine</p></div>';
 
 //building image highlights section
+
 const imgHighlights = document.createElement('div');
 imgHighlights.setAttribute('id', 'img-highlights');
+
+const imgSrc = document.createElement('img');
+
 
 
 
