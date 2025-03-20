237f881a00d4b1df5bb3f3ac86f980acb35301cc
pavat1x <pavatpichetsin@outlook.com>
2025-03-20 14:22:42 +0000

    finally implemented a working version of the homepage injection. Cleaned up object construction logic with the help of CoPilot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import { pageHero, imgHighlights, callToAction} from './pages/home';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const main = document.getElementById("content");
+    main.appendChild(pageHero);
+    main.appendChild(imgHighlights);    
+    main.appendChild(callToAction);    
+});
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,106 +1,60 @@
+import charsiu from "./charsiu.jpg"
+import siuyok from "./siuyok.jpg"
+import hofun from "./hofun.jpg"
+
+
 //Setting up section DOM objects
 
 
 //building hero section
 const pageHero = document.createElement('div');
 pageHero.setAttribute('id', 'page-hero');
-
 pageHero.innerHTML = '<div id="hero-text"><p id="hero-text-main">A <span>Premier</span> Cantonese Restaurant</p><p id="hero-text-sub">serving authentic Hong Kong style cuisine</p></div>';
 
 //building image highlights section
 
-class foodCard {
-    constructor(name, imgSrc, id, innerText) {
-        this.class = "food-card";
-        this.name = name;
-        this.imgSrc = imgSrc;
-        this.id = id;
-        this.innerText = innerText;
-    }
-
-    //get & set
-    get imgSrc() {
-        return this._imgSrc;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    get innerText() {
-        return this._innerText;
-    }
-
-    set imgSrc(newSrc) {
-        this._imgSrc = newSrc;
-    }
-
-    set id(newId) {
-        this._id = newId;
-    }
-
-    set innerText(newText) {
-        this._innerText = newText;
-    }
-
-    // methods
-    
-    compileCard = () => {
-        //building blocks
-        const divObj = document.createElement('div');
-        const imgContainer = document.createElement('img');
-        const txtContainer = document.createElement('div');
-        
-        
-        //Adding props
-        divObj.setAttribute('class', this.class);
-        divObj.setAttribute('id', `${this.id}-card`);
-        
-        imgContainer.src = this.imgSrc;
-        imgContainer.setAttribute('class', 'food-img');
-        imgContainer.setAttribute('id', `${this.id}-img`);
-
-        divObj.appendChild(imgContainer, txtContainer);
-
-        //todo: figure out how to efficiently set innerText as txtContainer's innerHTML
-        //maybe separate into a new method or just do it outside of the object bounds
-    }
-};
-
-function makeCard(name, imgSrc) {
-    const imgContainer = document.createElement('img');
-    const txtContainer = document.createElement('div');
-
-    divObj.setAttribute('class', 'food-card');
-    divObj.setAttribute('id', `${name}-card`);
-    
-    imgContainer.src = imgSrc;
-    imgContainer.setAttribute('class', 'food-img');
-    imgContainer.setAttribute('id', `${name}-img`);
-
-    const addHTML = (content) => imgContainer.innerHTML = content; 
-
-    const compileCard = () => {
-        const divObj = document.createElement('div');
-        divObj.appendChild(imgContainer, txtContainer);
-        return divObj;    
-    }
-    
-
-    return {compileCard, addHTML}
-
-}
-
-
-
 const imgHighlights = document.createElement('div');
 imgHighlights.setAttribute('id', 'img-highlights');
 
-const imgSrc = document.createElement('img');
+function makeImg(imgSrc, imgName, accomText) {
+    const foodCard = document.createElement('div'); 
+    const imgContainer = document.createElement('img');
+    const txtContainer = document.createElement('div');
 
+    foodCard.setAttribute('class', 'food-card');
+    foodCard.setAttribute('id', `${imgName}-card`); 
+
+    imgContainer.setAttribute('src', imgSrc);
+    imgContainer.setAttribute('class', 'food-img');
+    imgContainer.setAttribute('id', `${imgName}-img`); 
+    
+    txtContainer.setAttribute('class', 'accom-text');
+    txtContainer.innerHTML = accomText;
+
+    foodCard.appendChild(imgContainer);
+    foodCard.appendChild(txtContainer);
+
+    return {foodCard};
+
+}
+
+const charSiu = makeImg(charsiu, 'charsiu', '<h4>Char Siu Pork</h4><p>Pork, honey roasted to perfection.Served over rice or with vegetables.</p>');
+const siuYok = makeImg(siuyok, 'siuyok', '<h4>Siu Yok</h4><p>Chinese-style pork with a satisfying crackle.To be enjoyed with mustard.</p>'); 
+const hoFun = makeImg(hofun, 'hofun', '<h4>Beef Ho Fun</h4><p>A Hong Kong diner classic. Stir fried noodles with beef slices.Highly aromatic with a charred wok aftertaste.</p>');   
+
+imgHighlights.appendChild(charSiu.foodCard);
+imgHighlights.appendChild(siuYok.foodCard);
+imgHighlights.appendChild(hoFun.foodCard);
+
+
+//building up call to action section
+const callToAction = document.createElement('div');
+callToAction.setAttribute('id', 'call-to-action');
+
+callToAction.innerHTML = '<p id="action-text">Experience the Hong Kong diner magic today!</p><div id="contacts-container"><p>Call us at <span>1-800-HKDINER</span></p><p>Visit us at 121 Baltimore Road, NY</p></div>'
 
 
 
 //export DOM Objects
-export { pageHero, imgHighlights};
+export { pageHero, imgHighlights, callToAction};
 
