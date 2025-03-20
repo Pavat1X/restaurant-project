bc3709776e2f96e169ea4ab878aeb1909146319b
pavat1x <pavatpichetsin@outlook.com>
2025-03-20 14:32:53 +0000

    now that home page is finished, added home menu button logic

--- src/index.js
+++ src/index.js
@@ -5,5 +5,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const main = document.getElementById("content");
     main.appendChild(pageHero);
     main.appendChild(imgHighlights);    
-    main.appendChild(callToAction);    
+    main.appendChild(callToAction);
+    
+    const homeBtn = document.getElementById('home-btn');
+    const menuBtn = document.getElementById('menu-btn');    
+    const contactBtn = document.getElementById('contact-btn');
+
+    homeBtn.addEventListener('click', () => {
+        main.innerHTML = '';
+        main.appendChild(pageHero);
+        main.appendChild(imgHighlights);    
+        main.appendChild(callToAction);
+    });
+
+    
 });
